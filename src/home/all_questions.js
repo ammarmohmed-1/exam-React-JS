84a9c9929eee3afb9a35036af55861447c8c4ec2
ammarmohmed-1 <ammarmlmero@gmail.com>
2022-05-28 18:37:48 +0200

    third commit

--- src/home/all_questions.js
+++ src/home/all_questions.js
@@ -8,42 +8,42 @@ function All_Questions() {
         {/* one */}
         <div>
           <h5>~ Is Foodera Bread really baked fresh each day?</h5>
-          <p>
+          <article>
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia, there live the blind texts. Separated they
             live in Bookmarksgrove right at the coast of the Semantics, a large
             language.
-          </p>
+          </article>
         </div>
         {/* two */}
         <div>
           <h5>~ Can I order your products online?</h5>
-          <p>
+          <article>
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia, there live the blind texts. Separated they
             live in Bookmarksgrove right at the coast of the Semantics, a large
             language.
-          </p>
+          </article>
         </div>
         {/* three */}
         <div>
           <h5>~ Do you bake breads containing animal fats or products?</h5>
-          <p>
+          <article>
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia, there live the blind texts. Separated they
             live in Bookmarksgrove right at the coast of the Semantics, a large
             language.
-          </p>
+          </article>
         </div>
         {/* four */}
         <div>
           <h5>~ When are you opening a shop near me?</h5>
-          <p>
+          <article>
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia, there live the blind texts. Separated they
             live in Bookmarksgrove right at the coast of the Semantics, a large
             language.
-          </p>
+          </article>
         </div>
       </div>
     </div>
